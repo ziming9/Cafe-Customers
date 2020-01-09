c2e994d8beac843ec016f44b252d018928bbc970
ziming <ziye@ucsc.edu>
2020-01-09 15:07:52 -0800

    fix model location

--- routes/api/persons.js
+++ routes/api/persons.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-let Person = require('../models/lists-model');
+let Person = require('../../models/lists-model');
 
 // router.route('/').get(function(req, res) {
 //     Lists.find(function(err, lists) {
